016da937237a0ee9c2c4f2734ccdcf7cf63e4892
pengyue zhi <pengyuezhi@192.168.0.5>
2019-06-20 08:02:21 +0800

    初始化

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+// import Home from './views/Home.vue'
+const Home=()=>import('./views/Home.vue');
 Vue.use(Router)
 
 export default new Router({
